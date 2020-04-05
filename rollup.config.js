a8573fb17ed3aed891bc0360eabf9bf675fce4d4
Akella <kolesnik1310.dev@gmail.com>
2020-04-05 18:47:04 +0200

    Fixed script tag for right html output

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import htmlGenerate from 'rollup-plugin-generate-html-template';
 import css from 'rollup-plugin-css-only';
+import copy from 'rollup-plugin-copy';
 
 export default {
 	input: 'src/js/app.js',
@@ -19,18 +19,35 @@ export default {
 			file: 'dist/js/bundle.esm.js',
 			format: 'esm',
 		},
+		{
+			name: 'Piggie',
+			file: 'dist/js/bundle.iife.js',
+			format: 'iife',
+		},
 	],
 	plugins: [
 		resolve(),
 		babel({
 			exclude: 'node_modules/**',
 		}),
-		htmlGenerate({
-			template: 'src/index.html',
-			target: 'dist/app.html',
-		}),
 		css({
 			output: 'dist/css/bundle.css',
+		}),
+		copy({
+			targets: [
+				{
+					src: 'src/index.html',
+					dest: 'dist/',
+					rename: 'app.html',
+					transform: (contents) =>
+						contents
+							.toString()
+							.replace(
+								'<script src="app.js"></script>',
+								'<script src="js/bundle.iife.js"></script>'
+							),
+				},
+			],
 		}),
 	],
 };
